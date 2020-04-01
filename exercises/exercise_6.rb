998ebedead20369fbadade0e53a83c8dd5861075
Rahul Anthony Mitra <rahul.anthonym@gmail.com>
2020-04-01 17:45:28 +0000

    implement ex 6: 1-many relatioship and ex 7: validation checks

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -8,4 +8,16 @@ require_relative './exercise_5'
 puts "Exercise 6"
 puts "----------"
 
-# Your code goes here ...
+@store1.employees.create(first_name: "Nathan", last_name: "Drake", hourly_rate: 60)
+@store1.employees.create(first_name: "Samus", last_name: "Aran", hourly_rate: 40)
+@store1.employees.create(first_name: "Master", last_name: "Chief", hourly_rate: 65)
+@store1.employees.create(first_name: "Richter", last_name: "Belmont", hourly_rate: 60)
+@store1.employees.create(first_name: "Gordon", last_name: "Freeman", hourly_rate: 45)
+@store1.employees.create(first_name: "Booker", last_name: "Dewitt", hourly_rate: 70)
+@store2.employees.create(first_name: "Sam", last_name: "Porter", hourly_rate: 60)
+@store2.employees.create(first_name: "Solid", last_name: "Snake", hourly_rate: 55)
+@store2.employees.create(first_name: "Adam", last_name: "Jensen", hourly_rate: 60)
+@store2.employees.create(first_name: "Kiryu", last_name: "Kazuma", hourly_rate: 45)
+@store2.employees.create(first_name: "Andrew", last_name: "Ryan", hourly_rate: "abc")
+
+puts Employee.count
--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -9,4 +9,32 @@ require_relative './exercise_6'
 puts "Exercise 7"
 puts "----------"
 
-# Your code goes here ...
+class Employee < ActiveRecord::Base
+  belongs_to :store
+  validates :first_name, :last_name, presence: true
+  validates :hourly_rate, numericality: {only_integer: true}, :inclusion => {:in => 40..200}
+end
+
+
+class Store < ActiveRecord::Base
+  validates :name, length: {minimum: 3}
+  validates :annual_revenue, numericality: {only_integer: true, greater_than_or_equal_to: 0}
+  validate :must_carry_apparel
+
+  def must_carry_apparel
+    if (!mens_apparel && !womens_apparel)
+      errors.add(:mens_apparel, "must carry either")
+      errors.add(:womens_apparel, "must carry either")
+    end
+  end
+end
+
+puts "Enter new store name"
+
+@newemployee = Employee.create(first_name: "jon", last_name: "doe",hourly_rate: 35)
+puts @newemployee.errors.messages
+
+@newStore = Store.create(name: gets.chomp)
+puts @newStore.errors.messages
+
+
