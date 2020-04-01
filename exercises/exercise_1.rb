dddf878d680b98003ef410320dfbb2c845d155c3
Rahul Anthony Mitra <rahul.anthonym@gmail.com>
2020-04-01 04:44:02 +0000

    update exercise 4 with second query

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -7,8 +7,8 @@ puts "----------"
 # Richmond (annual_revenue of 1260000 carries women's apparel only)
 # Gastown (annual_revenue of 190000 carries men's apparel only)
 
-store = Store.create(name: "Burnaby", annual_revenue: 300000 , mens_apparel: true, womens_apparel: true)
-store = Store.create(name: "Richmond", annual_revenue: 1260000 , mens_apparel: false, womens_apparel: true)
-store = Store.create(name: "Gastown", annual_revenue: 190000 , mens_apparel: true, womens_apparel: false)
+Store.create(name: "Burnaby", annual_revenue: 300000 , mens_apparel: true, womens_apparel: true)
+Store.create(name: "Richmond", annual_revenue: 1260000 , mens_apparel: false, womens_apparel: true)
+Store.create(name: "Gastown", annual_revenue: 190000 , mens_apparel: true, womens_apparel: false)
 
 puts Store.count
--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -10,8 +10,18 @@ Store.create(name: "Surrey", annual_revenue: 224000 , mens_apparel: false, women
 Store.create(name: "Whistler", annual_revenue: 1900000, mens_apparel: true, womens_apparel: false)
 Store.create(name: "Yaletown", annual_revenue: 430000 , mens_apparel: true, womens_apparel: true)
 
-@mens_stores = Store.where(mens_apparel: true)
+puts Store.count
+@mens_stores = Store.where(mens_apparel: true, womens_apparel: false)
+
 
 for store in @mens_stores
   puts "#{store.name}: #{store.annual_revenue}"
 end
+
+@query2 = Store.where(["womens_apparel = :bool and annual_revenue < :rev", { bool: true, rev: 1000000 }])
+
+
+for store in @query2
+  puts "#{store.name}: #{store.annual_revenue}"
+end
+
