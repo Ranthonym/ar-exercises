e32705c02918e9ecc67ba53535c623fccad60b59
Rahul Anthony Mitra <rahul.anthonym@gmail.com>
2020-04-01 14:56:21 +0000

    implement exercise 5 with more queries

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -7,4 +7,7 @@ require_relative './exercise_4'
 puts "Exercise 5"
 puts "----------"
 
-# Your code goes here ...
+puts "total annual revenue: #{Store.sum("annual_revenue")}"
+puts "average annual revenue: #{Store.average("annual_revenue")}"
+
+puts "All stores over $1M annual revenue: #{Store.where(["annual_revenue > :rev", {rev: 1000000 }]).count}"
